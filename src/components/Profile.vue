1ad05d56133019c604434dc86f02faeab39edae3
rohit8450 <kartiksarwan2017.comp@mmcoe.edu.in>
2024-05-01 13:35:23 +0530

    Adding an Image Gallery

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Container from "./Container.vue";
 import UserBar from "./UserBar.vue";
+import ImageGallery from "./ImageGallery.vue";
 
 </script>
 
@@ -15,7 +16,14 @@ import UserBar from "./UserBar.vue";
                  following: 342
                }"
             />
-            <h1>IMAGE GALLERY</h1>
+            <ImageGallery :posts="[{
+              id: 1,
+              image: 'https://th.bing.com/th/id/OIP.383-9rJ6AFDKMg_G1L_6bQHaF-?w=237&h=191&c=7&r=0&o=5&pid=1.7'
+            }, 
+            {
+              id: 1,
+              image: 'https://th.bing.com/th/id/OIP.J6OmGXChgCnnKl2py3rKVgHaHR?rs=1&pid=ImgDetMain'
+            }]" />
         </div>
     </Container>
 </template>
